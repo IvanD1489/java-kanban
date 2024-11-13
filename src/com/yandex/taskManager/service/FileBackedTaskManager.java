90244f31dc307fc300913437e7e058ef4c40fc53
Ivan D <demin.ivan21@yandex.ru>
2024-11-13 16:54:12 +0300

    Edited constant

--- src/com/yandex/taskManager/service/FileBackedTaskManager.java
+++ src/com/yandex/taskManager/service/FileBackedTaskManager.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public class FileBackedTaskManager extends InMemoryTaskManager {
 
-    private final String FILE_DATA_PATH = "filewriter.txt";
+    private final static String FILE_DATA_PATH = "filewriter.txt";
 
     public FileBackedTaskManager() {
         File dir = new File(FILE_DATA_PATH);
