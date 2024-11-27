e2b72a8638813c5da8c7839e6cdf8f4f1cc15cb4
Ivan D <demin.ivan21@yandex.ru>
2024-11-27 23:36:27 +0300

    Code style

--- src/com/yandex/taskManager/Main.java
+++ src/com/yandex/taskManager/Main.java
@@ -5,14 +5,11 @@ import com.yandex.taskManager.service.Managers;
 import com.yandex.taskManager.service.TaskManager;
 
 public class Main {
-
     public static void main(String[] args) {
         System.out.println("Поехали!");
-        
         TaskManager taskManager = Managers.getFileBackedTaskManager("filewriter.txt");
         for (Task task : taskManager.getPrioritizedTasks()) {
             System.out.println(task);
         }
-
     }
 }
